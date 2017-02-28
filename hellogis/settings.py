f5c7d6ece61c9f20a00300bf9963e7c5fcc67699
Tristan Chambers <tristan.chambers@gmail.com>
2017-02-28 18:55:42 -0500

    enable special sauce

--- hellogis/settings.py
+++ hellogis/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     }
 }
 # DATABASES = {}
-# DATABASES['default'] = dj_database_url.config()
-# DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
